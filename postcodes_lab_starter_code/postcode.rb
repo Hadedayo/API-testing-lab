0450671474e54cacd93da1bfef5ce0d72041ff3b
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-27 20:09:43 +0000

    Used require_relative to access class in generate_data.rb. Created attr_accessor and initialize and get_single_postcode method

--- postcodes_lab_starter_code/postcode.rb
+++ postcodes_lab_starter_code/postcode.rb
@@ -1,17 +1,39 @@
 require 'httparty'
 require 'json'
+require_relative './lib/generate_data.rb'
 
 class Postcodesio
   include HTTParty
 
   base_uri 'https://api.postcodes.io'
 
-  def get_single_postcode(postcode)
-    JSON.parse(self.class.get("/postcodes/#{postcode}").body)
+  attr_accessor :single, :multiple
+
+  def initialize
+    @randomP = Postcodes.new
+    @single = @randomP.random_postcode
+    @multiple
+  end
+
+  def get_single_postcode
+    @single
   end
 
   def get_multiple_postcodes(postcodes_array)
-    JSON.parse(self.class.post('/postcodes', body: { "postcodes" => postcodes_array}).body)
+    @multiple = JSON.parse(self.class.post('/postcodes', body: { "postcodes" => postcodes_array}).body)
+  end
+
+  def get_status_from_postcode
+    @single['status']
+  end
+
+  def single_length_of_postcode
+   m =  @single['result']['postcode']
+   m.gsub(' ','')
   end
 
 end
+
+test = Postcodesio.new
+# puts test.single
+puts test.single_length_of_postcode
