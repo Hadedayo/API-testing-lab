75a5c4e06f4eaa8aecc93ba30189a954601c3b07
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-27 20:06:02 +0000

    Created .rb file in lib folder

---
+++ postcodes_lab_starter_code/lib/generate_data.rb
@@ -0,0 +1,21 @@
+require 'httparty'
+require "json"
+
+class Postcodes
+  include HTTParty
+
+  base_uri 'https://api.postcodes.io'
+
+  attr_accessor :postcode
+
+  def random_postcode
+    @postcode = JSON.parse(self.class.get("/random/postcodes").body)
+  end
+
+  def get_status_from_postcode
+    @single['status']
+  end
+end
+
+# test = Postcodes.new
+# puts test.random_postcode
