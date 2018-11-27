5e80db7afcafe7af7a74de9a928d492cdd9d767a
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-27 22:46:20 +0000

    Created methods in postcode.rb for nhs_authority, longitude, latitude, parliamentary, european electoral, primary care trust, region, parish, lsoa and msoa

--- postcodes_lab_starter_code/postcode.rb
+++ postcodes_lab_starter_code/postcode.rb
@@ -44,12 +44,54 @@ class Postcodesio
     @single['result']['northings']
   end
 
-  def four_uk_countries
+  def single_four_uk_countries
     @single['result']['country']
   end
+
+  def single_nhs_authority
+    @single['result']['nhs_ha']
+  end
+
+  def single_longitude_value
+    @single['result']['longitude']
+  end
+
+  def single_latitude_value
+    @single['result']['latitude']
+  end
+
+  def single_parliamentary_constituency
+    @single['result']['parliamentary_constituency']
+  end
+
+  def single_european_electoral_region
+    @single['result']['european_electoral_region']
+  end
+
+  def single_primary_care_trust
+    @single['result']['primary_care_trust']
+  end
+
+  def single_region
+    @single['result']['region']
+  end
+
+  def single_parish
+    @single['result']['parish']
+  end
+
+  def single_lsoa
+    @single['result']['lsoa']
+  end
+
+  def single_msoa
+    @single['result']['msoa']
+  end
+
+
 
 end
 
 test = Postcodesio.new
 # puts test.single
-puts test.four_uk_countries
+puts test.single_longitude_value
